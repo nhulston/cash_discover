443c743c04eb58737307ae222d4adfc15204c6d9
Arman Dave <adave@squareup.com>
2022-07-20 15:20:08 -0400

    pubspec merge

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,14 @@ import 'package:cash_discover/pages/coupons_page.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+Future<void> main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
